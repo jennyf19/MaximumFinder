8139042960b8407a81a46bb878e7fbe3e6f2aee7
jennyf19 <ferriesj@gmail.com>
2017-01-16 10:51:33 -0800

    add maximum value

--- MaximumFinder/Program.cs
+++ MaximumFinder/Program.cs
@@ -10,6 +10,33 @@ namespace MaximumFinder
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter three floating-point values,\n" +
+                " pressing 'Enter' after each one:");
+            double number1 = Convert.ToDouble(Console.ReadLine());
+            double number2 = Convert.ToDouble(Console.ReadLine());
+            double number3 = Convert.ToDouble(Console.ReadLine());
+
+            double result = Maximum(number1, number2, number3);
+
+            Console.WriteLine("Maximum is: {0}", result);
+            Console.ReadLine();
+        }
+
+        public static double Maximum(double x, double y, double z)
+        {
+            double maximumValue = x; 
+
+            if (y> maximumValue)
+            {
+                maximumValue = y;
+            }
+
+            if (z >maximumValue)
+            {
+                maximumValue = z;
+            }
+
+            return maximumValue;
         }
     }
 }
